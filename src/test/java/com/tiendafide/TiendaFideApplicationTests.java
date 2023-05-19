a905771286da95704349e43de53ea6adcc7553f9
StwartGF <114199940+StwartGF@users.noreply.github.com>
2023-05-19 12:48:53 -0600

    Primer commit del proyecto de la Tienda.

---
+++ src/test/java/com/tiendafide/TiendaFideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendafide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaFideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
