b225edb2c3e42146809e9b849c1de10cafd9f517
sgfof <sgfof@STWART08.conare.ac.cr>
2023-07-04 21:10:14 -0600

    Semana#6 uso de firebase y terminacion del crud de categoria

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -8,4 +8,14 @@ public interface CategoriaService {
     // Se obtiene un listado de categorias en un list
     public List<Categoria> getCategorias(boolean activos);
     
+    //se obtiene una categoria, a partir del id de un categoria
+    public Categoria getCategoria(Categoria categoria);
+    
+    //se inserta un nuevo categoria si el id del categoria esta vacio 
+    //se actualiza un categoria si el id del categoria no esta vacio
+    public void save(Categoria categoria);
+    
+    //se elimina el categoria que tiene el id pasado por parametro
+    public void delete (Categoria categoria);
+    
 }
--- src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -3,7 +3,9 @@ package com.tienda.service.impl;
 import com.tienda.dao.CategoriaDao;
 import com.tienda.domain.Categoria;
 import com.tienda.service.CategoriaService;
+import static io.grpc.InternalChannelz.id;
 import java.util.List;
+import static org.hibernate.query.results.Builders.entity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +24,21 @@ public class CategoriaServiceImpl implements CategoriaService {
             lista.removeIf(categoriaEletemento -> !categoriaEletemento.isActivo() );
         }
         return lista;        
+    }
+    @Override
+    @Transactional(readOnly= true)
+    public Categoria getCategoria(Categoria categoria){
+       return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+    @Override
+    @Transactional
+    public void save (Categoria categoria){
+        categoriaDao.save(categoria);
+    }
+    @Override
+    @Transactional
+    public void delete(Categoria categoria){
+        categoriaDao.delete(categoria);
     }
     
 }
